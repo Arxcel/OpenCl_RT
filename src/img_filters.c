ed89e3356016863aa800496f9708f6912ad11444
Andrew <andrey.faraponov@gmail.com>
2018-03-19 19:22:58 +0200

    save

--- src/img_filters.c
+++ src/img_filters.c
@@ -6,80 +6,80 @@
 /*   By: afarapon <afarapon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 13:37:03 by afarapon          #+#    #+#             */
-/*   Updated: 2018/03/19 18:43:37 by afarapon         ###   ########.fr       */
+/*   Updated: 2018/03/19 19:16:21 by afarapon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rt.h"
 
-// static void			set_smooth(t_main *main)
-// {
-// 	byte[] inputBytes = BitmapBytes.GetBytes(input);
-// 	byte[] outputBytes = new byte[inputBytes.Length];
+static void			set_smooth(t_main *main)
+{
+	// unsigned int	*input_pixels = main->sdl.img.pixels;
+	// unsigned int	*output_pixels = (unsigned int*)malloc(sizeof(int) * main->sdl.img.h * main->sdl.img.w);
 
-// 	int width = input.Width;
-// 	int height = input.Height;
+	// size_t			width = main->sdl.img.w;
+	// size_t			height = main->sdl.img.h;
 
-// 	int kernelWidth = kernel.GetLength(0);
-// 	int kernelHeight = kernel.GetLength(1);
+	// int kernelWidth = kernel.GetLength(0);
+	// int kernelHeight = kernel.GetLength(1);
 
-// 	//Производим вычисления
-// 	for (int x = 0; x < width; x++)
-// 	{
-// 		for (int y = 0; y < height; y++)
-// 		{
-// 			double rSum = 0, gSum = 0, bSum = 0, kSum = 0;
+	// //Производим вычисления
+	// for (int x = 0; x < width; x++)
+	// {
+	// 	for (int y = 0; y < height; y++)
+	// 	{
+	// 		double rSum = 0, gSum = 0, bSum = 0, kSum = 0;
 
-// 			for (int i = 0; i < kernelWidth; i++)
-// 			{
-// 				for (int j = 0; j < kernelHeight; j++)
-// 				{
-// 					int pixelPosX = x + (i - (kernelWidth / 2));
-// 					int pixelPosY = y + (j - (kernelHeight / 2));
-// 					if ((pixelPosX < 0) || 
-// 					(pixelPosX >= width) || (pixelPosY < 0) || (pixelPosY >= height))
-// 						continue;
+	// 		for (int i = 0; i < kernelWidth; i++)
+	// 		{
+	// 			for (int j = 0; j < kernelHeight; j++)
+	// 			{
+	// 				int pixelPosX = x + (i - (kernelWidth / 2));
+	// 				int pixelPosY = y + (j - (kernelHeight / 2));
+	// 				if ((pixelPosX < 0) || 
+	// 				(pixelPosX >= width) || (pixelPosY < 0) || (pixelPosY >= height))
+	// 					continue;
 
-// 					byte r = inputBytes[3 * (width * pixelPosY + pixelPosX) + 0];
-// 					byte g = inputBytes[3 * (width * pixelPosY + pixelPosX) + 1];
-// 					byte b = inputBytes[3 * (width * pixelPosY + pixelPosX) + 2];
+	// 				byte r = inputBytes[3 * (width * pixelPosY + pixelPosX) + 0];
+	// 				byte g = inputBytes[3 * (width * pixelPosY + pixelPosX) + 1];
+	// 				byte b = inputBytes[3 * (width * pixelPosY + pixelPosX) + 2];
 
-// 					double kernelVal = kernel[i, j];
+	// 				double kernelVal = kernel[i, j];
 
-// 					rSum += r * kernelVal;
-// 					gSum += g * kernelVal;
-// 					bSum += b * kernelVal;
+	// 				rSum += r * kernelVal;
+	// 				gSum += g * kernelVal;
+	// 				bSum += b * kernelVal;
 
-// 					kSum += kernelVal;
-// 				}
-// 			}
+	// 				kSum += kernelVal;
+	// 			}
+	// 		}
 
-// 			if (kSum <= 0)
-// 				kSum = 1;
-// 			//Контролируем переполнения переменных
-// 			rSum /= kSum;
-// 			if (rSum < 0)
-// 				rSum = 0;
-// 			if (rSum > 255)
-// 				rSum = 255;
+	// 		if (kSum <= 0)
+	// 			kSum = 1;
+	// 		//Контролируем переполнения переменных
+	// 		rSum /= kSum;
+	// 		if (rSum < 0)
+	// 			rSum = 0;
+	// 		if (rSum > 255)
+	// 			rSum = 255;
 
-// 			gSum /= kSum;
-// 			if (gSum < 0)
-// 				gSum = 0;
-// 			if (gSum > 255)
-// 				gSum = 255;
+	// 		gSum /= kSum;
+	// 		if (gSum < 0)
+	// 			gSum = 0;
+	// 		if (gSum > 255)
+	// 			gSum = 255;
 
-// 			bSum /= kSum;
-// 			if (bSum < 0) bSum = 0;
-// 			if (bSum > 255) bSum = 255;
+	// 		bSum /= kSum;
+	// 		if (bSum < 0) bSum = 0;
+	// 		if (bSum > 255) bSum = 255;
 
-// 			//Записываем значения в результирующее изображение
-// 			outputBytes[3 * (width * y + x) + 0] = (byte)rSum;
-// 			outputBytes[3 * (width * y + x) + 1] = (byte)gSum;
-// 			outputBytes[3 * (width * y + x) + 2] = (byte)bSum;
-// 		}
-// 	}
-// }
+	// 		//Записываем значения в результирующее изображение
+	// 		outputBytes[3 * (width * y + x) + 0] = (byte)rSum;
+	// 		outputBytes[3 * (width * y + x) + 1] = (byte)gSum;
+	// 		outputBytes[3 * (width * y + x) + 2] = (byte)bSum;
+	// 	}
+	// }
+}
 
 static unsigned int		set_sepia(unsigned int in)
 {
